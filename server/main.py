06a497ecb69c9a489d913fab3b4c8ea38a3a6df6
unolife <unolife92@gmail.com>
2020-06-22 12:13:56 +0900

    predict 3 days

--- server/main.py
+++ server/main.py
@@ -4,12 +4,9 @@ import pymysql
 import config 
 import json
 import pandas as pd
-import lightgbm as lgb
-import xgboost as xgb
-from sklearn.linear_model import Lasso
-from sklearn.ensemble import GradientBoostingRegressor
 import keras
 import pickle
+import time
 
 app = Flask(__name__)
 conf = config.dbconfig
@@ -29,18 +26,20 @@ def index():
     print(rows)
     return json.dumps(rows)
 
+
 @app.route('/predict')
 def predict():
+    start = time.time()
     pepper_price = [10300, 10700, 10900, 11300, 11500, 11000, 10700]
     garlic_price = [3300, 3700, 4200, 4500, 4500, 4500, 4500]
-    onion_price = [7700, 7300, 7100, 7000, 7200, 77000, 8300]
+    onion_price = [7700, 7300, 7100, 7000, 7200, 7700, 8300]
 
     product = ['pepper','garlic','onion']
 
     models = ["XGBoost", "GradientBoosting", "LightGBM", "Lasso"]
 
+    product_price_day1 = []
     for p in product:
-        result = {}
         if p == 'pepper':
             price = pepper_price
         elif p == 'garlic':
@@ -48,19 +47,112 @@ def predict():
         elif p == 'onion':
             price = onion_price
 
-        input_dict = {"f0":[2000], "f1":[6], "f2": [20]}
+        input_dict = {"f0":[2020], "f1":[6], "f2": [20]}
         for i in range(len(price)):
             input_dict["f{}".format(i+3)] = [price[i]]
 
         input = pd.DataFrame(input_dict)
-        print(input)
+
         # 10 models per each method(10 seed data)
+        total_list = []
         for i in range(10):
+            each_iter = []
             for m in models:
                 ml_model = pickle.load(open("./models/{}/pima_{}_{}.pickle.dat".format(p, i, m), "rb"))
-                result['{}_{}_{}'.format(p,m,i)] = ml_model.predict(input)
+                prediction = int(ml_model.predict(input))
+                each_list = price[1:]
+                each_list.append(prediction)
+                each_iter.append(each_list)
 
-        print(result)  
+            keras_list = price[1:]
+                
+            k_model = keras.models.load_model('./models/{}/keras_{}.hdf5'.format(p,i))
+            prediction = int(k_model.predict(input))
+            
+            keras_list.append(prediction)
+            each_iter.append(each_list)
+            
+            total_list.append(each_iter)
+        product_price_day1.append(total_list)
+
+    print('day1 done')
+
+    product_price_day2 = []
+    for p in range(len(product_price_day1)):
+        total_list = []
+        for i in range(10):
+            each_iter = []
+            for m in range(len(models)):
+                input_dict = {"f0":[2020], "f1":[6], "f2": [20]}
+                for j in range(len(product_price_day1[p][i][m])):
+                    input_dict["f{}".format(j+3)] = [product_price_day1[p][i][m][j]]
+                
+                input = pd.DataFrame(input_dict)
+
+                ml_model = pickle.load(open("./models/{}/pima_{}_{}.pickle.dat".format(product[p], i, models[m]), "rb"))
+                prediction = int(ml_model.predict(input))
+                each_list = product_price_day1[p][i][m][1:]
+                each_list.append(prediction)
+                each_iter.append(each_list)
+
+            input_dict = {"f0":[2020], "f1":[6], "f2": [20]}
+            for j in range(len(product_price_day1[p][i][4])):
+                input_dict["f{}".format(j+3)] = [product_price_day1[p][i][4][j]]
+
+            input = pd.DataFrame(input_dict)
+
+            keras_list = product_price_day1[p][i][4][1:]
+                
+            k_model = keras.models.load_model('./models/{}/keras_{}.hdf5'.format(product[p],i))
+            prediction = int(k_model.predict(input))
+            
+            keras_list.append(prediction)
+            each_iter.append(each_list)
+            
+            total_list.append(each_iter)
+        product_price_day2.append(total_list)
+
+    print('day2 done')
+
+
+    product_price_day3 = []
+    for p in range(len(product_price_day2)):
+        total_list = []
+        for i in range(10):
+            each_iter = []
+            for m in range(len(models)):
+                input_dict = {"f0":[2020], "f1":[6], "f2": [20]}
+                for j in range(len(product_price_day2[p][i][m])):
+                    input_dict["f{}".format(j+3)] = [product_price_day2[p][i][m][j]]
+                
+                input = pd.DataFrame(input_dict)
+
+                ml_model = pickle.load(open("./models/{}/pima_{}_{}.pickle.dat".format(product[p], i, models[m]), "rb"))
+                prediction = int(ml_model.predict(input))
+                each_list = product_price_day2[p][i][m][1:]
+                each_list.append(prediction)
+                each_iter.append(each_list)
+
+            input_dict = {"f0":[2020], "f1":[6], "f2": [20]}
+            for j in range(len(product_price_day2[p][i][4])):
+                input_dict["f{}".format(j+3)] = [product_price_day2[p][i][4][j]]
+
+            input = pd.DataFrame(input_dict)
+
+            keras_list = product_price_day2[p][i][4][1:]
+                
+            k_model = keras.models.load_model('./models/{}/keras_{}.hdf5'.format(product[p],i))
+            prediction = int(k_model.predict(input))
+            
+            keras_list.append(prediction)
+            each_iter.append(each_list)
+            
+            total_list.append(each_iter)
+        product_price_day3.append(total_list)
+
+    print('day3 done')
+    print(product_price_day3)
+    print('time: ', time.time() - start)
     return '0'
 
 if __name__ == '__main__':
